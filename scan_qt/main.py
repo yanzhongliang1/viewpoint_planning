bc46ecbc6f63fdf2e91ab1ded0364388161e9a12
YZL <1071526648@qq.com>
2025-11-13 22:20:07 +0800

    初代版本，未重构

---
+++ scan_qt/main.py
@@ -0,0 +1,12 @@
+# scan_qt/main.py
+from scan_qt.views.main_window import MainWindow
+from PyQt5.QtWidgets import (
+    QApplication
+)
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
